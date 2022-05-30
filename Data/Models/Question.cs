ac89ded5f31a25ab3aeba38118efbd7250bc0988
Yordan <daka7a11@abv.bg>
2022-05-30 15:47:57 +0300

    .

--- Data/Models/Question.cs
+++ Data/Models/Question.cs
@@ -1,7 +1,27 @@
-﻿namespace TheHotel.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+using TheHotel.Common;
+
+namespace TheHotel.Data.Models
 {
     public class Question : BaseDeletableModel<int>
     {
+        [Required]
+        [MinLength(GlobalConstants.ClientNameMinLength)]
+        [MaxLength(GlobalConstants.ClientNameMaxLength)]
+        public string FirstName { get; set; }
 
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        [MinLength(GlobalConstants.TitleMinLength, ErrorMessage = GlobalConstants.TitleLengthErrorMsg)]
+        public string Title { get; set; }
+
+        [Required]
+        [MinLength(GlobalConstants.MessageTextMinLength, ErrorMessage = GlobalConstants.MessageLengthErrorMsg)]
+        public string Text { get; set; }
     }
 }
--- Data/Models/Review.cs
+++ Data/Models/Review.cs
@@ -1,7 +1,27 @@
-﻿namespace TheHotel.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+using TheHotel.Common;
+
+namespace TheHotel.Data.Models
 {
     public class Review : BaseDeletableModel<int>
     {
+        [Required]
+        [MinLength(GlobalConstants.ClientNameMinLength)]
+        [MaxLength(GlobalConstants.ClientNameMaxLength)]
+        public string FirstName { get; set; }
 
+        [Required]
+        [MinLength(GlobalConstants.ClientNameMinLength)]
+        [MaxLength(GlobalConstants.ClientNameMaxLength)]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public int Rating { get; set; }
+
+        [Required]
+        [MinLength(GlobalConstants.MessageTextMinLength, ErrorMessage = GlobalConstants.MessageLengthErrorMsg)]
+        public string Text { get; set; }
     }
 }
